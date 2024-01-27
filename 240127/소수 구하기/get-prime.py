967c6fb9f498bfdc65db58bb21fa539f7b3d3fe0
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-01-27 19:01:31 +0900

    Title: 소수 구하기, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240127/소수 구하기/get-prime.py
+++ 240127/소수 구하기/get-prime.py
@@ -1,13 +1,14 @@
 n= int(input())
-if n>1:
-    for i in range(2,n+1):
-        isSosu = True
-        #소수확인
-        if i ==2:
-            print(i,end=" ")
-            continue
-        for j in range(2,n):
-            if i%j ==0:
-                isSosu=False
-        if isSosu:
-            print(i,end=" ")
+
+# 1~n까지수 
+for i in range(1,n+1):
+    isSosu = True
+    #i가 소수인지 확인 => 1과 자기자신만을 약수로,, 나머기로 나누어떨어지면 소수가 아님 
+    
+    
+    for j in range(2,n):
+        if i!=j and i%j == 0: # 왜 i!=j  조건 필수인지.. 없으면 왜 아무것도 안뜨는지 모르겠음 
+            isSosu=False
+    #소수면 i출력 
+    if i!=1 and isSosu:
+        print(i,end=" ")
