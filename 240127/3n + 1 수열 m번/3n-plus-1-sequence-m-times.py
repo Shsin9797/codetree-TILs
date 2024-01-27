7e4ae7d534e21f6e0def7224c810a37ddc587f85
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-01-27 17:19:08 +0900

    Title: 3n + 1 수열 m번, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240127/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,15 @@
+m = int(input())
+for _ in range(m):
+    n= int(input())
+
+
+    cnt =0 
+
+    while n>1:
+        if n %2 ==0:
+            n /= 2
+        else:
+            n = n*3 +1
+
+        cnt +=1
+    print(cnt)
