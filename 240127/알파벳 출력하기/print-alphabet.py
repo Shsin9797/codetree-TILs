c377d2d74319eab7008fc2f13cc28b9e2664b346
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-01-27 12:24:18 +0900

    Title: 알파벳 출력하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240127/알파벳 출력하기/print-alphabet.py
+++ 240127/알파벳 출력하기/print-alphabet.py
@@ -6,4 +6,8 @@ for i in range(n): #행
     for j in range(0,i+1): #열 i 는 0부터 시작함에 주의
         print(chr(cnt),end="")
         cnt += 1
+        if cnt > ord("Z"):
+            cnt = ord("A")
     print()
+
+# 알파벳 다시 돌아가서 A 부터해야함에 주의
