044836915a80d61aec5d20c12e789d2107ffdf15
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-01-29 08:49:24 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240129/피보나치 수/fibonacci-number.py
@@ -0,0 +1,12 @@
+# DP 테이블 정의: F[i] : = F[1]부터 F[i-1]까지의 수를 더한값  
+# 초깃값 정의 F[1] =1, F[2]=1 , F[3]=2
+# 점화식 F[i]= F[i-1]+(i-1)
+
+n=int(input())
+F=[0 for _ in range(n+1)]
+F[1]=1
+F[2]=1
+
+for i in range(3,n+1):
+    F[i] = F[i-1]+(i-1)
+print(F[n])
