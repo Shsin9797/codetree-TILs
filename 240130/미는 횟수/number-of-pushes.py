fa128a5fc96399fe11e294b692792f393f901753
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-01-30 08:30:11 +0900

    Title: 미는 횟수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/미는 횟수/number-of-pushes.py
+++ 240130/미는 횟수/number-of-pushes.py
@@ -3,9 +3,16 @@ B=input()
 
 n= 0 
 
-while A!=B: #### 불가능한 경우 체크하려면 이거 하면 안됨.. 
+
+"""while A!=B: #### 불가능한 경우 체크하려면 이거 하면 안됨.. 
     A = A[-1]+A[:-1]
     n+=1
+    """
 
+for i in range(len(A)-1):
+    A=A[-1]+A[:-1]
+    n +=1 
+    if A==B:
+        break
 
 print(n)
