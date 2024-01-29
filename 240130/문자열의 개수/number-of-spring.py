b98e815a536f6271649108c8b1ab3f3da9d7ef78
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-01-30 08:26:20 +0900

    Title: 문자열의 개수, Time: 227ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/문자열의 개수/number-of-spring.py
+++ 240130/문자열의 개수/number-of-spring.py
@@ -2,12 +2,14 @@ li = []
 cnt = 0
 while True:
     a= input()# 받는걸 반복문 안에 넣었어야했음 
-    cnt += 1 
+    
     if a =='0':
         break
     
+    cnt += 1  # 더해주는 위치주의 
+
     if cnt%2 ==1:
         li.append(a)
 
-print(cnt)
+print(cnt) #뭘 출력하는지 주의 
 print(*li,sep="\n")
