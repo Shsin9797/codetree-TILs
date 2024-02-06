d67e3ef08c8e30899cd8b08e40d44c6852724469
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-06 12:25:13 +0900

    Title: 1이 되는 순간까지, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 240206/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -1,10 +1,14 @@
 def f(x):#x가 1이될때까지 2로 나누는 함수 
     #종료조건 
-    if x<= 1 :
-        return -1
+    if x== 1 :
+        return 0
+    elif x ==0:
+        return 0
     # 재귀 : f(x//2) 부르고 cnt +=1 
-    n= f(x//2)+1
-   
+    if x %2 ==0:
+        n= f(x//2)+1
+    else:
+        n=f(x//3)+1
     return n
 
 
