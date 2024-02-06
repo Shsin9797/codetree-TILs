a4906e02ad548a51066bac9f9b950e9f2929daa6
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-07 08:17:12 +0900

    Title: 사칙연산 함수, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240207/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,36 @@
+def checkMun(x):
+    if x in ['+','-','/','*']:
+        return True     
+    else:
+        return False
+    
+
+def plus(x,y):
+    return x+y
+
+def minus(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x//y
+
+
+x,m,y= input().split()
+x,y = int(x),int(y)
+
+if not checkMun(m):
+    print('False')
+else:
+    print(f"{x} {m} {y} = ",end="")
+
+if m =='+':
+    print(plus(x,y))
+elif m=='-':
+    print(minus(x,y))
+elif m=='/':
+    print(divide(x,y))
+elif m == '*':
+    print(multiply(x,y))
