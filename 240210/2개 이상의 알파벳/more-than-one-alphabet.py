e4254130796e0b2a4e56c0c30980c096230004ad
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-10 23:40:43 +0900

    Title: 2개 이상의 알파벳, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240210/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240210/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,13 +1,12 @@
 def isTwo(x):
     a= x[0]
-    
     for i in range(len(x)):
-        if x[i] != a:
-            return True
-    
-    return False
 
-A= input().split()
+        if x[i] != a:
+            return True    
+
+#A= list(input().split()) <-split 이 안먹음..
+A =input()
 
 if isTwo(A):
     print("Yes")
