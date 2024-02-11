f20b702eb1acef52954ffa4a756d37638d664273
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-11 23:50:40 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240211/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240211/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -3,10 +3,14 @@ mo = input()
 
 def f():
     num =-1 
+    
     for n in range(len(arr)-len(mo)+1):
+        flag = True
         for m in range(len(mo)):
             if arr[n+m] != mo[m]:
+                flag = False
                 break
+        if flag:
             num = n 
     return num
 
