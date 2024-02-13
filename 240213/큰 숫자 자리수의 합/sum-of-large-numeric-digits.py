d884f6b1119df3f809acf9feaeffbcfdbbf46ed1
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-13 12:23:27 +0900

    Title: 큰 숫자 자리수의 합, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
+++ 240213/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
@@ -1,12 +1,11 @@
 a,b,c = map(int,input().split())
 target = str(a*b*c)
-print(target)
+
 
 def f(idx):# 0~ (idx-1) 번째까지 자리값을 더하여 반환 
     #종료조건
     if idx ==1: #idx 는 길이를 넣을거라.. 0이 안됨.. 0이면 0 반환해야함 
         return int(target[0])
-    print(target[idx-1])
     #재귀호출 
     return f(idx-1)+int(target[idx-1])
 
