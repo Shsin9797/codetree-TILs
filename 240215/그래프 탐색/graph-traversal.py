5bef1b5a5cccb0ac8cb893c2fffeee0969afa933
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-15 08:50:12 +0900

    Title: 그래프 탐색, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/그래프 탐색/graph-traversal.py
@@ -0,0 +1,27 @@
+n,m= map(int,input().split())
+
+#정점리스트 생성 
+vertex = [[] for _ in range(n+1)]
+#방문 리스트 생성
+visited = [0 for _ in range(n+1)]
+
+
+#DFS 함수 생성 
+def DFS(v):#방문 정점 지정하면 연관된거 다 찾아줌 
+    for i in vertex[v]:
+        if not visited[i]:
+            visited[i] =1 
+            DFS(i)
+
+
+
+#간선 체크 
+for i in range(m):
+    x,y = map(int,input().split())
+    vertex[x].append(y)
+    vertex[y].append(x)
+
+#dfs 탐색 
+DFS(1)
+
+print(sum(visited)-1)
