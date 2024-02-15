0c3d9b13e2bf9f9eea4fa645470481e3cb128309
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-16 08:21:16 +0900

    Title: 두 개의 동일한 수열, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240216/두 개의 동일한 수열/two-equal-series.py
+++ 240216/두 개의 동일한 수열/two-equal-series.py
@@ -9,3 +9,20 @@ if A==B:
     print("Yes")
 else:
     print("No")
+
+
+"""
+zip(리스트1,리스트2) 해서 순서대로 묶은다음에
+다시 언패킹해서 
+각 원소를 비교할수도있군.. 
+
+def equal():
+    # n개의 원소를 순서대로 봤을 때
+    # 전부 동일한 경우에만 일치합니다.
+    # 단 하나라도 다르다면, false입니다.
+    for elem1, elem2 in zip(a, b):
+        if elem1 != elem2:
+            return False
+    
+    return True
+"""
