1fdca017714f73c0462b8237298be872bc72fb73
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-20 23:59:21 +0900

    Title: 비오는 날, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240220/비오는 날/rainy-day.py
+++ 240220/비오는 날/rainy-day.py
@@ -1,4 +1,25 @@
+class Weather:
+    def __init__(self,date,day,weather):
+        self.date=date
+        self.day=day
+        self.weather=weather
+
 n= int(input())
+nlist=[]
 for _ in range(n):
     date,day,weather = input().split()
-    w = Weather()
+    w = Weather(date,day,weather)
+    if w.weather =="Rain":
+        nlist.append(w)
+
+
+short = nlist[0].date 
+idx=0
+for i in range(n):
+    w= nlist[i]
+    if w.date < short: 
+        short = w.date
+        idx =i 
+
+p = nlist[idx]
+print(p.date,p.day,p.weather)
