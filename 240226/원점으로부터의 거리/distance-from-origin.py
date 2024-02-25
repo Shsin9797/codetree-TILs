7b83d8e5d3407188a5fe59e5ba7ef9eea0dcd1ae
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-26 08:40:58 +0900

    Title: 원점으로부터의 거리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240226/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,19 @@
+cnt =0 
+
+class Spot:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        global cnt
+        cnt +=1
+        self.cnt =cnt
+
+n= int(input())
+spotLi =[]
+for _ in range(n):
+    spotLi.append(Spot(*input().split()))
+
+spotLi.sort(key= lambda j: (j.x+j.y,j.cnt)
+
+for k in spotLi:
+    print(k.cnt)
