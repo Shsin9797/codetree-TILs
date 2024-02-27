a4794e7811d9f3c61b3bbb5c2bb015b96ffdebee
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-28 08:35:09 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240228/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = map(int,input().split())
+y2011 = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+yoil = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+idxY = 0
+
+while True:
+    if m1==m2 and d1==d2:
+        break
+    
+    d1 +=1 
+    idxY +=1
+
+    if d1 > y2011[m1]:
+        m1 +=1
+        d1 = 1 
+    
+    if idxY >= len(yoil):
+        idxY =0
+
+
+print(yoil[idxY])
