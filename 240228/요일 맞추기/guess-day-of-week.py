7126cb7c7839b7ad67802f6e8dde12fb224dadf0
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-28 08:48:20 +0900

    Title: 요일 맞추기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240228/요일 맞추기/guess-day-of-week.py
+++ 240228/요일 맞추기/guess-day-of-week.py
@@ -7,15 +7,54 @@ while True:
     if m1==m2 and d1==d2:
         break
     
-    d1 +=1 
-    idxY +=1
+    #두번째가 큰 경우 값을 증가시켜준다 
+    if m1 < m2 : 
+        d1 +=1 
+        idxY +=1
+    elif m1 == m2 and d1 <d2:
+        d1 +=1 
+        idxY +=1
+    elif m1 ==m2 and d1> d2 :
+        d1 -=1
+        idxY -=1
+    elif m1>m2 :
+        d1 -=1
+        idxY-=1
 
+    #값의 범위 체크
     if d1 > y2011[m1]:
         m1 +=1
+        
+        #월도 체크해주기...
+        if m1 >12: 
+            m1 =1
+        elif m1 < 1:
+            m1 =12
+            
         d1 = 1 
+
+    elif d1 <1:
+        m1 -=1
+        
+        #월도 체크해주기...
+        if m1 >12: 
+            m1 =1
+        elif m1 < 1:
+            m1 =12
+        
+        d1 = y2011[m1]
     
+    #월도 체크해주기...
+    if m1 >12: 
+        m1 =1
+    elif m1 < 1:
+        m1 =12
+    
+    #요일 법위 체크
     if idxY >= len(yoil):
         idxY =0
+    elif idxY <0:
+        idxY =6
 
 
 print(yoil[idxY])
