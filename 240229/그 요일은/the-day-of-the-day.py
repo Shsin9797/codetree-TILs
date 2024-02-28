d617e75ffaef66d91a2b61eda509020fc14fc812
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-02-29 08:23:38 +0900

    Title: 그 요일은, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240229/그 요일은/the-day-of-the-day.py
+++ 240229/그 요일은/the-day-of-the-day.py
@@ -1,9 +1,10 @@
 yoil = ["Mon", 'Tue', 'Wed', "Thu", "Fri", 'Sat', 'Sun']
 dayNum = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+
 m1,d1,m2,d2 =map(int,input().split())
 A = input()
-kIdx = 0 
 
+kIdx = 0 
 aIdx = yoil.index(A)
 aCnt = 0 
 
@@ -16,13 +17,15 @@ while True :
         break
     
     # 값변경
+    #일 값 변경
     d1 +=1 
-    if d1 >= dayNum[m1]:
+    if d1 > dayNum[m1]: 
         m1 +=1
         d1 =1
-    aIdx +=1 
-    if aIdx >= len(yoil):
-        aIdx =0 
+    #요일 값 변경 
+    kIdx +=1  #aIdx 가 아니라 kIdx 를 변경해야한다
+    if kIdx >= len(yoil):
+        kIdx =0 
     
 
 
