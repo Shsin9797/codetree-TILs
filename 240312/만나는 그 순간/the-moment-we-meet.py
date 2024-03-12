93da1af8604eba22e3f82026e9d1b9dca3222e23
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-03-12 12:11:37 +0900

    Title: 만나는 그 순간, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240312/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,39 @@
+n,m =map(int,input().split())
+A=[]
+B=[]
+
+#시작점 
+aP=0
+bP=0
+
+A.append(aP)
+B.append(bP)
+
+#A 이동
+for _ in range(n):
+    d,t = input().split()
+    t=int(t)
+    for _ in range(t):
+        if d =='L':
+            aP -= 1
+        else:
+            aP += 1
+        A.append(aP)
+
+
+#B이동
+for _ in range(m):
+    d,t = input().split()
+    t=int(t)
+    for _ in range(t):
+        if d=='L':
+            bP -= 1
+        else:
+            bP += 1
+        B.append(bP)
+
+#만나는 지점 찾기 
+for k in range(1,len(A)):
+    if A[k] == B[k]:
+        print(k)
+        break
