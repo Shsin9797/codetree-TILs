1cc11017bb79287389e7807b96ed2c6ed2e379c8
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-03-30 18:23:06 +0900

    Title: 거울에 레이저 쏘기 2, Time: 240ms, Memory: 48MB, Status: Passed - CodeTree

--- 240330/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240330/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,3 +1,4 @@
+#886, 2862
 n= int(input())
 arr=[]
         #위,오,아래,왼
@@ -24,7 +25,7 @@ k= int(input())
 # 방향
 # 1~n: 아래 (2) / n+1~2n: 왼 (3) / 2n+1~3n: 위 (0) / 3n~ 4n-1 : 오른 (1)
 # 넘버 (dy,dx) 
-# (0,0~n-1)       / (0~n-1, n-1)  / (n-1, n-1~0)    / (n-1~0, 0)
+# (0,0~n-1) :증가  / (0~n-1, n-1) :증가  / (n-1, n-1~0) : x감소   / (n-1~0, 0) : y 감소
 if 1<=k <=n:
     b= 2
     y,x = 0, k-1
@@ -33,10 +34,10 @@ elif n+1<=k<=2*n:
     y,x = k-n-1, n-1
 elif 2*n+1 <=k <= 3*n:
     b=0
-    y,x = n-1, k-2*n-1
+    y,x = n-1, 3*n -k
 elif 3*n+1 <=k <=4*n:
     b=1
-    y,x =  k-3*n-1 , 0
+    y,x =  4*n-k , 0
 
 #범위 벗어나는 거 찾기
 def inRange(dy,dx):
@@ -45,7 +46,7 @@ def inRange(dy,dx):
 #진행하기 
 cnt =0  
 
-while inRange(y,x):#범위내인지 확인 
+while True:#범위내인지 확인 
     #print('x',x,'y',y,'b',b)
     #거울 찾기 
     m = arr[y][x]
@@ -53,6 +54,9 @@ while inRange(y,x):#범위내인지 확인
     cnt+=1 # 거울 한번 마주침 
     b= changeDirect(m,b) #방향 먼저 바꾸고 
     x,y = x+dxs[b], y+dys[b]#위치변경 
+    if not inRange(x,y):
+        #print('x',x,'y',y)
+        break
     
 
 print(cnt)
