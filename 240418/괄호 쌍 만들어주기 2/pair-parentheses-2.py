d4c2c7d64a7513213722af03c99b8836f76a8431
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-18 07:31:27 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240418/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240418/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,24 +1,24 @@
-# 변수 선언 및 입력
-string = input()
-n = len(string)
+A=input()
+lA= len(A)
+cnt=0
 
-# 모든 쌍을 다 잡아봅니다.
-cnt = 0
-for i in range(n - 1):
-    for j in range(i + 1, n - 1):
-        if string[i] == '(' and string[i + 1] == '(' and string[j] == ')' and string[j + 1] == ')':
-            cnt += 1
-            
+for i in range(lA-3): # 여기 2아니라 3빼줘야하는거 맞나..
+    if A[i]=='('and A[i+1]=='(':
+        for j in range(i+2,lA-1):
+            if A[j]==')'and A[j+1] ==')':
+                cnt+=1
+
 print(cnt)
 
-# A=input()
-# lA= len(A)
-# cnt=0
+# # 변수 선언 및 입력
+# string = input()
+# n = len(string)
 
-# for i in range(lA-3): # 여기 2아니라 3빼줘야하는거 맞나..
-#     if A[i]=='('and A[i+1]=='(':
-#         for j in range(i+2,lA-1):
-#             if A[j]==')'and A[j+1] ==')':
-#                 cnt+=1
-
+# # 모든 쌍을 다 잡아봅니다.
+# cnt = 0
+# for i in range(n - 1):
+#     for j in range(i + 1, n - 1):
+#         if string[i] == '(' and string[i + 1] == '(' and string[j] == ')' and string[j + 1] == ')':
+#             cnt += 1
+            
 # print(cnt)
