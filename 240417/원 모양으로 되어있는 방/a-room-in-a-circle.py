f2c9efd9945b64d4fbc2f43a5e644bcc05b87c06
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-17 23:59:11 +0900

    Title: 원 모양으로 되어있는 방, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240417/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240417/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -2,19 +2,30 @@ import sys
 
 n= int(input())
 li= [int(input()) for _ in range(n)]
-max_len = 0 #-sys.maxsize()
-for s in range(n): #start
-    sum_len=0
-    for e in range(n):#end
-        if e < s:
-            print('시작점이 더 크다',n-abs(s-e))
-            sum_len += (n-abs(s-e))*li[e]
-        elif s < e:
-            print(abs(s-e))
-            sum_len += abs(s-e)*li[e]
+min_len = sys.maxsize #-sys.maxsize()
 
-    print('sumLen',sum_len) 
-    max_len =max(max_len,sum_len)
+for s in range(n): #start
+
+    sum_len=0
     
 
-print(max_len)
+    for e in range(n):#end 계속 바꿔야함 end를 바꿔가며,, 사람거리 구하기 
+        idx=s
+        go= (e-s+n)%n #두거리의 차이 
+        
+        if go ==0 : #이동거리가 없는경우 구하지 않는다.
+            continue # break 하면 해당 s 자리에서 시작하는거 구하는거 끝남..   e 가 안돌아서..
+        
+        
+        for walk in range(go):
+            idx = (idx+1)%n
+            sum_len += (walk+1)*li[idx]
+        #print('(s,e): ',s,e,'go',go)
+        #print(idx+1,'번 방에 있는 사람수',li[idx])
+
+    print('(s,e): ',s+1,e+1,'go',go) 
+    print('sumLen',sum_len) 
+    if sum_len:
+        min_len= min(min_len,sum_len)
+    
+print(min_len) #min 값 구하는거임
