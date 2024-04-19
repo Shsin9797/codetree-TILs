b1c1dfbdcdb6f4f18b0b653f2ae8de3ee26eaab6
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-19 20:19:24 +0900

    Title: G or H 3, Time: 186ms, Memory: 28MB, Status: Passed - CodeTree

--- 240419/G or H 3/G-or-H-3.py
+++ 240419/G or H 3/G-or-H-3.py
@@ -16,7 +16,7 @@ for _ in range(n):
 max_sum = -sys.maxsize
 
 # 합 완전탐색 
-for i in range(1,10001-k+1): # 체킹 시작점 # +1 해주기
+for i in range(1,10001-k+1): # 체킹 시작점 # +1 해주기 # 끝점은 n-k 아님.. arr 길이가 ..10001 임에 주의 ..
     sum_num = sum(arr[i:i+k+1]) # +1 해주기- 이게 첫점이랑 끝점 둘다 포괄하는거라
     max_sum = max(max_sum,sum_num)
 
