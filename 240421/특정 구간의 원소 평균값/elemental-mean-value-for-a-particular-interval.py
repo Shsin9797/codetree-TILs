6d69496e3ca3e9ccbdfd8f0bf110c1afd554d928
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-21 14:19:02 +0900

    Title: 특정 구간의 원소 평균값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240421/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -0,0 +1,15 @@
+#import statistics 
+
+n= int(input())
+arr= list(map(int,input().split()))
+
+cnt = 0 
+for i in range(n):
+    for j in range(i,n):
+        avg = statistics.mean(arr[i:j+1])
+        for k in range(i,j+1):
+            if avg == arr[k]:
+                cnt +=1
+                break
+
+print(cnt)
