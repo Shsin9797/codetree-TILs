aea13934b1c833c762995c5161ab81040cb37643
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-21 17:34:28 +0900

    Title: 아름다운 수열 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240421/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,19 @@
+n,m= map(int,input().split())
+A = list(map(int,input().split()))
+B= list(map(int,input().split()))
+
+cnt =0 
+
+for i in range(n-m+1):#A의 시작점 
+    #돌면서 B값 삭제
+    BB = B[:] 
+    for k in range(i,i+m):
+        for t in range(m):
+            if A[k] == BB[t]:
+                BB[t]=0
+    
+    #BB 값 다삭제되면 cnt 값 증가 
+    if sum(BB)==0:
+        cnt+=1
+
+print(cnt)
