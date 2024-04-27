5d1e380c4aadf1f0968a14ddf0d0fc0a0cec4feb
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-27 14:37:47 +0900

    Title: 개발자의 능력 3, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240427/개발자의 능력 3/ability-of-developer-3.py
+++ 240427/개발자의 능력 3/ability-of-developer-3.py
@@ -8,7 +8,7 @@ min_cha= sys.maxsize
 for i in range(6):
     for j in range(i+1,6):
         for k in range(j+1,6):
-            sum_abil1=0
+            #sum_abil1=0  없어도 될듯 
             if i != j and j !=k and i != k : 
                 sum_abil1 = arr[i] + arr[j] +arr[k]
                 sum_abil2 = sum_arr - sum_abil1
