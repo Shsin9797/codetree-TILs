118d2119b4ce803833e717bde5e7f4bf07ec4656
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-27 15:24:05 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 166ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240427/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,51 @@
+n= int(input())
+
+a1,b1,c1 =map(int,input().split())
+a2,b2,c2 = map(int,input().split())
+
+def inn(a1,a):
+    #print('a1',a1,'a',a)
+
+    if abs(a1-a) <= 2:
+        return True 
+    
+    if a1 == n-1 :
+        if a== 0 or a==1:
+            return True 
+
+    if a1 == 0 : 
+        if a==(n-1) or a==(n-2) :
+            return True
+    
+    if a1 == 1 : 
+        if a == (n-1):
+            return True
+
+    if a1 == (n-2) :
+        if a== 0 :
+            return True
+
+    return False
+
+
+def able(a,b,c):
+    # global a1,b1,c1,a2,b2,c2
+
+    if inn(a1,a) and inn(b1,b) and inn(c1,c):
+        return True
+
+    if inn(a2,a) and inn(b2,b) and inn(c2,c):
+        return True
+    
+    return False
+
+cnt =0 
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+
+            if able(i,j,k):
+                cnt +=1
+
+print(cnt)
