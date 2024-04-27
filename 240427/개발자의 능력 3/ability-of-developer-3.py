3f36566c7131047b52861baa57a52db1e5a086dd
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-04-27 14:37:18 +0900

    Title: 개발자의 능력 3, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240427/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,18 @@
+import sys
+
+arr = list(map(int,input().split()))
+sum_arr= sum(arr)
+
+min_cha= sys.maxsize
+
+for i in range(6):
+    for j in range(i+1,6):
+        for k in range(j+1,6):
+            sum_abil1=0
+            if i != j and j !=k and i != k : 
+                sum_abil1 = arr[i] + arr[j] +arr[k]
+                sum_abil2 = sum_arr - sum_abil1
+                cha_abil = abs(sum_abil2 - sum_abil1)
+                min_cha= min(min_cha,cha_abil)
+
+print(min_cha)
