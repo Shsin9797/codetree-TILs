a843780d1075a8481dbf4f2dc797ef5fd16bbfd2
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-05-03 18:27:08 +0900

    Title: 삼각형 만들기, Time: 105ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240503/삼각형 만들기/create-triangle.py
+++ 240503/삼각형 만들기/create-triangle.py
@@ -10,37 +10,25 @@ def isSam(i1,i2,i3):
     x2,y2 = i2
     x3,y3 = i3
     
-    ga=0
-    se=0
-
-    if x1==x2 :
-        se= abs(y3-y2)
-    elif x2==x3 :
-        se = abs(y2-y1)
-    elif x1==x3:
-        se = abs(y1-y2)
-
-    if y1==y2 :
-        ga= abs(x3-x2)
-    elif y2==y3 :
-        ga = abs(x2-x1)
-    elif y1==y3:
-        ga = abs(x1-x2)
+    if x1 != x2 and x2 != x3:
+        return 0
     
+    if y1 != y2 and y2 != y3 : 
+        return 0 
+
+    area = abs((x1*y2+x2*y3+x3*y1)-(x2*y1+x3*y2+x1*y3))
+    return area
+
     
-    if ga and se :
-        return ga*se 
-    else:
-        return ga*se
 
 #점 세개 구하기
 li=[]
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
-            print(spots[i],spots[j],spots[k])
+            #print(spots[i],spots[j],spots[k])
             li.append(isSam(spots[i],spots[j],spots[k]))
-print(li)
+#print(li)
 
 #최대넓이에 2 곱한값 출력 
 print(max(li))
