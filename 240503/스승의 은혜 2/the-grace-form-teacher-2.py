450e1f94f29fbc1ff39c145db5d54855dc48698f
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-05-03 19:59:09 +0900

    Title: 스승의 은혜 2, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240503/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,29 @@
+n,b =map(int,input().split())
+p=[0]*n
+
+#선물가격 세팅
+for i in range(n):
+    p[i] =int(input())
+
+max_cnt = 0 
+#반값할인할 애 정하기 
+for i in range(n):
+    #초기화 
+    pp= p[:]
+    bb=b
+    #반값할인
+    p[i] /=2
+    #오름차순 정렬
+    pp.sort() #sort(pp)
+    #명수 세기 
+    cnt = 0 
+    for k in range(n):
+        bb -= pp[k]
+        if bb >= 0:
+            cnt +=1 
+        else:
+            break
+    
+    max_cnt =max(max_cnt,cnt)
+
+print(max_cnt)
