a3a0e592947a402bd42c2cd80d5b73843f68619f
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-05-03 19:47:05 +0900

    Title: 겹치지 않는 선분 2, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240503/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -3,31 +3,30 @@ n= int(input())
 arr=[]
 for _ in range(n):
     x1,x2 = map(int,input().split())
-    arr.append(((x1,0),(x2,1)))
+    arr.append((x1,x2))
 #print(arr)
 #끝점이 .. . x1은 다른 직선에 비해 작은데 x2는 크던지.. 
 cnt = 0 
 
 for i in range(n):#직선 1개 고르기 
+    
     parall= True
-    for j in range(i+1, n): #다른 직선 1개 고르기 
-        if i != j : 
+    
+    #for 문 
+    for j in range(n): #다른 직선 1개 고르기 
+        if i == j : #등호... 
             continue
-        print(arr[i])
-        x1,x2 = arr[i][0][0],arr[i][1][0]
-        print('x1',x1,'x2',x2)
-        x3,x4= arr[j][0][0],arr[j][1][0]
-        print('x3',x3,'x4',x4)
+        #print(i,':',arr[i])
+        x1,x2 = arr[i]
+        #print(j,':',arr[j])
+        x3,x4 = arr[j] # j여야했다... 
+        #print('--------')
+        
+        if ((x1 < x3 and x2 > x4) or (x1 > x3 and x2 < x4 )):
+            parall = False
+            break
 
-        if x1 < x3 and x2>x4:
-            parall =False
-            break
-        elif x1 > x3 and x2 < x4: 
-            parall =False
-            break
-            
     if parall:
-        print(i,j ,'parall')
         cnt +=1 
 
 print(cnt)
