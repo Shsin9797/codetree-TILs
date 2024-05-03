a75ea62a561af9fe2634d717a760cf98f9418e5b
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-05-03 19:10:57 +0900

    Title: 운행 되고 있는 시간, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240503/운행 되고 있는 시간/hours-in-service.py
@@ -0,0 +1,28 @@
+import sys 
+
+n= int(input())
+time= []
+#개발자 시간 세팅
+for _ in range(n):
+    A,B =map(int,input().split())
+    time.append((A,B))
+
+max_time= -sys.maxsize
+#count0=[0]*1001
+#개발자 한명씩 빼기 
+
+for i in range(n):#뺄 개발자
+    #count=count0[::]
+    count =0 
+    for j in range(n): #개발자 순회 
+        if i ==j : 
+            continue 
+        s,e= time[j]
+        print(s,e)
+        for t in range(s,e):
+            count += 1 
+        print(count)
+    print()
+    max_time = max(max_time,count)
+
+print(max_time)
