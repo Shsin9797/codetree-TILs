dccadd66e5275045b5a7f455072e6b6792029847
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-05-03 20:31:23 +0900

    Title: 스승의 은혜 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240503/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -4,30 +4,45 @@ p=[0]*n
 #선물가격 세팅
 for i in range(n):
     p[i] =int(input())
-
+    
+p=sorted(p)
 max_cnt = 0 
+
 #반값할인할 애 정하기 
 for i in range(n):
     #초기화 
     pp= p[:]
     bb=b
+    cnt = 0
+
     #반값할인
-    p[i] /=2
+    pp[i] /=2 #p 가 아니라 pp 를 절반띄기 
+
     #오름차순 정렬
-    #pp.sort() #sort(pp)
-    #print(pp)
+    pp.sort() #sort(pp)
     
-    #명수 세기 
-    cnt = 0 
+    #명수 세기    
     for k in range(n):
         bb -= pp[k]
         if bb >= 0:
-            cnt +=1
+            cnt =k+1
             continue 
         else:
             #cnt= k
             break
     
+    if max_cnt == cnt :
+        break 
+
     max_cnt =max(max_cnt,cnt)
 
+'''
+원본을 오름차순 정렬했을때 
+14, 22, 32, 34, 70, 98, 128, 128, 130, 140, 140, 156, 156 이순서로 나오고, 
+첫 10개인14, 22, 32, 34, 70, 98, 128, 128, 130, 140 를 합하면 
+796으로, 예산인 863과67 만큼 차이가 납니다. 
+
+이때 140 을 반값 하면 70이 되고, 
+14, 22, 32, 34, 70,70, 98, 128, 128, 130, 140 까지 해서 
+'''
 print(max_cnt)
