861109dada6af89b1f03d55730881362c6225243
Cindy <93774025+Shsin9797@users.noreply.github.com>
2024-05-17 08:17:05 +0900

    Title: 등차수열, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240517/등차수열/arithmetic-sequence.py
+++ 240517/등차수열/arithmetic-sequence.py
@@ -1,12 +1,15 @@
 n= int(input())
 li = list(map(int,input().split()))
 
-cnt = 0 
+max_cnt = 0
+ 
 for k in range(1,102):
+    cnt = 0
     for i in range(n):
         for j in range(i+1,n):
             if (li[j]-k) == (k-li[i]):
                 cnt +=1  
+    max_cnt=max(cnt,max_cnt)
 
 
-print(cnt)
+print(max_cnt)
